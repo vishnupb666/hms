00b3845cfffbb1f7df2f991ec9f9a69deb431127
VISHNU PB <vishnupb666@gmail.com>
2022-11-10 18:15:14 +0530

    Signed-off-by: VISHNU PB <vishnupb666@gmail.com>

--- hmsapp/admin.py
+++ hmsapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import role,department,user_ext,doctor,appointment,leave
 # Register your models here.
+admin.site.register(user_ext)
+admin.site.register(role)
+admin.site.register(department)
+admin.site.register(doctor)
+admin.site.register(appointment)
+admin.site.register(leave)
